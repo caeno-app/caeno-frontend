1a6a6848e8a6278ea96d00825ca5a9d0f5f6288f
Ryan Yang <ryanqyang@gmail.com>
2020-01-16 11:23:25 -0800

    Updates

---
+++ precache-manifest.cc5226315f3cd406970a7136eef8e36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7396bf3098f3b550fc2071e6eb25f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b5fa1686335a0b399b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "557675d531a771c2fdf6",
+    "url": "/static/js/2.25938a00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.25938a00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75b5fa1686335a0b399b",
+    "url": "/static/js/main.61812253.chunk.js"
+  },
+  {
+    "revision": "6f0575d42bc04f2967f5",
+    "url": "/static/js/runtime-main.444fa3bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
