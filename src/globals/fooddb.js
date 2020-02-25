207804039e0907aa03a5a7ccd20c14d2580edadf
Ryan Yang <ryanqyang@gmail.com>
2020-02-25 15:12:50 -0800

    updated endpoint to the actual domain api endpoint

--- src/globals/fooddb.js
+++ src/globals/fooddb.js
@@ -2,7 +2,7 @@
 /**
  * @TODO setup https for server
  */
-const endpoint = `http://172.112.215.241`;
+const endpoint = `http://api.caeno.app`;
 
 /**
  * @documentation - https://github.com/caeno-app/caeno-backend
